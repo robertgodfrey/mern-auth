e0204f2c65ad5da7d069202c709fff0e63b33b6a
Rob Godfrey <rob_godfrey@outlook.com>
2023-06-08 16:59:56 -1000

    Add auth, finish user API

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,11 +2,24 @@ import asyncHandler from 'express-async-handler';
 import generateToken from "../utils/generateToken.js";
 import User from '../models/userModel.js';
 
-// @desc    Auth user/set token
+// @desc    Log in user/set token
 // route    POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'auth user' });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 // @desc    Register a new user
@@ -44,21 +57,45 @@ const registerUser = asyncHandler(async (req, res) => {
 // route    POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'logout user' });
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+    res.status(200).json({ message: 'Successfully logged out' });
 });
 
 // @desc    Get user profile
 // route    GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'user profile' });
+    const user = {
+        name: req.user.name,
+        email: req.user.email
+    };
+    res.status(200).json(user);
 });
 
 // @desc    Update user profile
 // route    PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'update user profile' });
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            name: updatedUser.name,
+            email: updatedUser.email
+        });
+    }
 });
 
 export {
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
@@ -10,9 +11,9 @@ connectDB();
 const port = process.env.PORT || 5000;
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
